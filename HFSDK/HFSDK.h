67241470ac445581659896e760eff3dd47aa06d9
HeartlessSoy <hainanlu@uniondrug.cn>
2019-04-23 17:28:31 +0800

    Initial Commit

---
+++ HFSDK/HFSDK.h
@@ -0,0 +1,19 @@
+//
+//  HFSDK.h
+//  HFSDK
+//
+//  Created by HeartlessSoy on 2019/4/23.
+//  Copyright © 2019 HeartlessSoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HFSDK.
+FOUNDATION_EXPORT double HFSDKVersionNumber;
+
+//! Project version string for HFSDK.
+FOUNDATION_EXPORT const unsigned char HFSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HFSDK/PublicHeader.h>
+
+
